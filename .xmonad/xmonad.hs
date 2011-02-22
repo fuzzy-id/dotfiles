91f8e228ce761f292d47f7b871ffa76e65cfa649
vince <bachth[ed]uni-mainz.de>
2011-02-22 21:36:59 +0100

    serious basic config :)

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,9 +1,20 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
+import XMonad.Hooks.ManageDocks
+import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.EZConfig(additionalKeys)
+import System.IO
 
-main = xmonad =<< xmobar myConfig
-
-myConfig = defaultConfig {
-  terminal = "urxvt",
-  modMask = mod4Mask,
-  normalBorderColor = "black" }
+main = do
+     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
+     xmonad $ defaultConfig
+     	    { manageHook = manageDocks <+> manageHook defaultConfig
+	    , layoutHook = avoidStruts $ layoutHook defaultConfig
+	    , logHook = dynamicLogWithPP $ xmobarPP
+	      	      	{ ppOutput = hPutStrLn xmproc
+			, ppTitle = xmobarColor "green" "" . shorten 50
+			}
+	    , terminal = "urxvt"
+	    , modMask = mod4Mask
+	    , normalBorderColor = "black"
+	    }

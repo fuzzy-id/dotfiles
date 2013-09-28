e26754b75659ecebb2781d3ad1c2c4d10adebe19
Thomas Bach <thbach@students.uni-mainz.de>
2013-09-28 14:05:45 +0200

    using proper xmonad function

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -5,15 +5,10 @@ import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
-main = do
-     xmproc <- spawnPipe "xmobar ~/.xmobarrc"
-     xmonad $ defaultConfig
+main = xmonad =<< xmobar myConfig
+myConfig =  defaultConfig
      	    { manageHook = manageDocks <+> manageHook defaultConfig
 	    , layoutHook = avoidStruts $ layoutHook defaultConfig
-	    , logHook = dynamicLogWithPP $ xmobarPP
-	      	      	{ ppOutput = hPutStrLn xmproc
-			, ppTitle = xmobarColor "green" "" . shorten 50
-			}
 	    , terminal = "urxvtc"
 	    , modMask = mod4Mask
 	    , normalBorderColor = "black"

7347f7023d1693514453c435edd279772ec1eea0
Thomas Bach <thbach@students.uni-mainz.de>
2014-03-14 10:26:04 +0100

    throwing out unnecessary stuff

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,14 +1,15 @@
 import XMonad
 import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.Script (execScriptHook)
 
+main :: IO ()
 main = xmonad =<< xmobar myConfig
-myConfig =  defaultConfig
-     	    { manageHook = manageDocks <+> manageHook defaultConfig
-	    , layoutHook = avoidStruts $ layoutHook defaultConfig
-	    , terminal = "urxvtc"
-	    , modMask = mod4Mask
-	    , normalBorderColor = "black"
-	    , startupHook = execScriptHook ""
-	    }
+
+myConfig :: XConfig (Choose Tall (Choose (Mirror Tall) Full))
+myConfig =  defaultConfig { terminal = "urxvtc"
+                          , modMask = mod4Mask
+                          , normalBorderColor = "black"
+                          , startupHook = execScriptHook ""
+                          }
+
+

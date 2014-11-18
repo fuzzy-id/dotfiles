2a4cbb35569c2d4a5597fc231cd2426f9c44b82a
Thomas Bach <thbach@students.uni-mainz.de>
2014-11-18 07:13:17 +0100

    gardening, fixed too greedy set-default parser

--- .xmonad/Pulse.hs
+++ .xmonad/Pulse.hs
@@ -15,47 +15,53 @@ data PulseItem = Sink { sinkName :: String
                       , sinkMute :: Bool
                       } deriving (Show,Eq)
 
+defaultPulseItem :: PulseItem
 defaultPulseItem = Sink "" False 0 False
 
+createSinks :: [(String,PulseItem -> PulseItem)] -> [PulseItem]
 createSinks = map createSink . groupBySinkName
-  where createSink l@((name,_):_) = 
+  where createSink l@((name,_):_) =
           foldr ($) defaultPulseItem ((\s -> s {sinkName = name}):(map snd l))
 
-groupBySinkName = 
-  groupBy ((==) `on` fst) 
-  . sortBy (compare `on` fst) 
+groupBySinkName :: [(String, b)] -> [[(String, b)]]
+groupBySinkName =
+  groupBy ((==) `on` fst)
+  . sortBy (compare `on` fst)
   . filter ((sinkName defaultPulseItem /=) . fst)
 
 -- Commands
-   
+
+paDump :: IO String
 paDump = readProcess "pacmd" ["dump"] ""
 
 -- Parsing
+getRight :: Show a => Either a b -> b
 getRight = either (error . show) id
 
 pDump = pSinkRelated `P.sepEndBy` P.string "\n"
 
-pSinkRelated = P.try pSinkMute 
-               P.<|> P.try pSinkVolume 
+pSinkRelated = P.try pSinkMute
+               P.<|> P.try pSinkVolume
                P.<|> P.try pSinkDefault
                P.<|> (P.many (P.noneOf "\n") *> pure (sinkName defaultPulseItem,id))
 
-pSinkDefault = (,(\s -> s {sinkDefault = True})) 
+pSinkDefault = (,(\s -> s {sinkDefault = True}))
                <$> (P.string "set-default-sink "
                     *> P.many (P.noneOf "\n"))
-pSinkMute = P.string "set-sink-mute " 
-            *> ((,) <$>  P.anyChar `P.manyTill` P.space) 
+pSinkMute = P.string "set-sink-mute "
+            *> ((,) <$>  P.anyChar `P.manyTill` P.space)
             <*> ((\m s -> s {sinkMute = m }) <$> pYesOrNo)
-pSinkVolume = P.string "set-sink-volume " 
-              *> ((,) <$>  P.anyChar `P.manyTill` P.string " 0x") 
-              <*> ((\v s -> s {sinkVolume = v}) . readHex 
+pSinkVolume = P.string "set-sink-volume "
+              *> ((,) <$>  P.anyChar `P.manyTill` P.string " 0x")
+              <*> ((\v s -> s {sinkVolume = v}) . readHex
                    <$> P.many P.alphaNum)
-pYesOrNo = (P.string "yes" *> pure True) 
+pYesOrNo = (P.string "yes" *> pure True)
            P.<|> (P.string "no" *> pure False)
 
+readHex :: (Num c, Read c) => String -> c
 readHex = fst . readHex'
   where readHex' [] = (0,0)
-        readHex' (c:xs) 
+        readHex' (c:xs)
           | c == 'a' = calc 10
           | c == 'b' = calc 11
           | c == 'c' = calc 12

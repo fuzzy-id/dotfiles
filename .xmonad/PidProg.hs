e60db3fd39631ca2c986cd1222486d6e6026c751
Thomas Bach <thbach@students.uni-mainz.de>
2015-01-06 22:40:07 +0100

    major cleanup; purged almost all the calls to io

--- .xmonad/PidProg.hs
+++ .xmonad/PidProg.hs
@@ -2,13 +2,17 @@
 module PidProg where
 
 import Codec.Binary.UTF8.String
+import Control.Applicative
 import Control.Exception
-import Control.Monad
 import System.Directory
 import System.FilePath
 import System.Posix.Process
 import System.Posix.Types
 import XMonad
+
+makePidProg :: FilePath -> [String] -> Bool -> PidProg
+makePidProg command args respawn = PidProg{..}
+  where pidFile = ""
 
 data PidProg = PidProg { command :: String
                        , args    :: [String]
@@ -17,25 +21,48 @@ data PidProg = PidProg { command :: String
                        }
                        deriving (Show,Read,Eq)
 
-commandName :: PidProg -> String
-commandName = takeFileName . command
+runPidProgs :: MonadIO m => [PidProg] -> m ()
+runPidProgs ps = io $ do pidPath <- initPidPath
+                         let progs = map (setPidFile pidPath) ps
+                         mapM_ runPidProg progs 
 
-makePidProg :: FilePath -> [String] -> Bool -> PidProg
-makePidProg command args respawn = PidProg{..}
-  where pidFile = ""
+initPidPath :: IO FilePath
+initPidPath = do pidPath <- (</> ".xmonad" </> "run") <$> getHomeDirectory
+                 createDirectoryIfMissing True pidPath
+                 return pidPath
 
-readPidFile :: (Read r, MonadIO m) => PidProg -> m r
-readPidFile prog = liftM read $ (io . readFile . pidFile) prog
+runPidProg :: PidProg -> IO ()
+runPidProg prog = do
+  pid <- pidProgPid prog
+  case (respawn prog,pid) of
+    (_,    Nothing  ) -> spawnPidProg prog
+    (False,Just _   ) -> return ()
+    (True, Just pid') -> respawnPidProg prog pid'
 
-writePidFile :: (Show a, MonadIO m) => PidProg -> a -> m () 
-writePidFile prog pid = io $ writeFile (pidFile prog) (show pid)
+spawnPidProg :: PidProg -> IO ()
+spawnPidProg prog = spawnPID' >>= writePidFile prog
+  where spawnPID' = xfork $ executeFile 
+                              (encodeString . command $ prog)
+                              True 
+                              (map encodeString $ args prog)
+                              Nothing
+
+respawnPidProg :: (Show a) => PidProg -> a -> IO ()
+respawnPidProg prog pid = do spawn ("kill " ++ show pid)
+                             spawnPidProg prog
+
+readPidFile :: (Read r) => PidProg -> IO r
+readPidFile PidProg{..} = read <$> readFile pidFile
+
+writePidFile :: (Show a) => PidProg -> a -> IO () 
+writePidFile PidProg{..} = writeFile pidFile . show
 
 setPidFile :: FilePath -> PidProg -> PidProg   
-setPidFile path prog = 
-  prog {pidFile = path </> commandName prog <.> "pid"}
+setPidFile path prog = prog {pidFile = path </> commandName <.> "pid"}
+  where commandName = (takeFileName . command) prog
 
-pidProgPid :: MonadIO m => PidProg -> m (Maybe ProcessID)
-pidProgPid prog = do fileExists <- (io . doesFileExist . pidFile) prog
+pidProgPid :: PidProg -> IO (Maybe ProcessID)
+pidProgPid prog = do fileExists <- (doesFileExist . pidFile) prog
                      if not fileExists
                         then return Nothing
                         else do oldPid <- readPidFile prog 
@@ -44,39 +71,7 @@ pidProgPid prog = do fileExists <- (io . doesFileExist . pidFile) prog
                                    then return (Just oldPid)
                                    else return Nothing
 
-runPidProgs :: MonadIO m => [PidProg] -> m ()
-runPidProgs ps = do pidPath <- initPidPath
-                    let progs = map (setPidFile pidPath) ps
-                    mapM_ runPidProg progs 
-
-spawnPidProg :: MonadIO m => PidProg -> m ()
-spawnPidProg prog = io (spawnPID' prog >>= writePidFile prog)
-  where spawnPID' p = xfork $ executeFile 
-                                (encodeString . command $ p) 
-                                True 
-                                (map encodeString $ args p)
-                                Nothing
-
-respawnPidProg :: (Show a, MonadIO m) => PidProg -> a -> m ()
-respawnPidProg prog pid = do spawn ("kill " ++ show pid)
-                             spawnPidProg prog
-
-doesPidProgRun :: MonadIO m => ProcessID -> m Bool
-doesPidProgRun pid = 
-  do result <- io (try $ getProcessPriority pid :: IO (Either IOException Int))
-     case result of
-       Right _ -> return True
-       Left _ -> return False
-
-runPidProg :: MonadIO m => PidProg -> m ()
-runPidProg prog = do
-  pid <- pidProgPid prog
-  case (respawn prog,pid) of
-    (_,    Nothing  ) -> spawnPidProg prog
-    (False,Just _   ) -> return ()
-    (True, Just pid') -> respawnPidProg prog pid'
-
-initPidPath :: MonadIO m => m FilePath
-initPidPath = do pidPath <- liftM (</> ".xmonad" </> "run") $ io getHomeDirectory
-                 io $ createDirectoryIfMissing True pidPath
-                 return pidPath
+doesPidProgRun :: ProcessID -> IO Bool
+doesPidProgRun pid =
+  either (const False) (const True) 
+  <$> (try $ getProcessPriority pid :: IO (Either IOException Int))
